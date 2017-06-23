1b6815774300bc8e1c56c7266465f82e9666bf53
Jean-Marcellin Truong <jean-marcellin.truong@ensimag.grenoble-inp.fr>
2017-06-23 17:42:02 +0200

    Added source files
    Extraction, training and segmentation of patches are working

---
+++ workspace/src/util.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
+from skimage import morphology, color, io, exposure
+
+def IoU(y_true, y_pred):
+    """Returns Intersection over Union score for ground truth and predicted masks."""
+    assert y_true.dtype == bool and y_pred.dtype == bool
+    y_true_f = y_true.flatten()
+    y_pred_f = y_pred.flatten()
+    intersection = np.logical_and(y_true_f, y_pred_f).sum()
+    union = np.logical_or(y_true_f, y_pred_f).sum()
+    return (intersection + 1) * 1. / (union + 1)
+
+def Dice(y_true, y_pred):
+    """Returns Dice Similarity Coefficient for ground truth and predicted masks."""
+    assert y_true.dtype == bool and y_pred.dtype == bool
+    y_true_f = y_true.flatten()
+    y_pred_f = y_pred.flatten()
+    intersection = np.logical_and(y_true_f, y_pred_f).sum()
+    return (2. * intersection + 1.) / (y_true.sum() + y_pred.sum() + 1.)
+
+def masked(img, gt, mask, alpha=1):
+    # TODO : handle channels
+    """Returns image with GT lung field outlined with red, predicted lung field
+    filled with blue."""
+    rows, cols, _ = img.shape
+    color_mask = np.zeros((rows, cols, 3))
+    boundary = morphology.dilation(gt, morphology.disk(3)) - gt
+    color_mask[mask == 1] = [0, 0, 1]
+    color_mask[boundary == 1] = [1, 0, 0]
+    img_color = img
+
+    img_hsv = color.rgb2hsv(img_color)
+    color_mask_hsv = color.rgb2hsv(color_mask)
+
+    img_hsv[..., 0] = color_mask_hsv[..., 0]
+    img_hsv[..., 1] = color_mask_hsv[..., 1] * alpha
+
+    img_masked = color.hsv2rgb(img_hsv)
+    return img_masked
+
+def remove_small_regions(img, size):
+    """Morphologically removes small (less than size) connected regions of 0s or 1s."""
+    img = morphology.remove_small_objects(img, size)
+    img = morphology.remove_small_holes(img, size)
+    return img
+
+def normalize(array):
+    """Return normalized array"""
+    array -= array.mean()
+    array /= array.std()
+    return array
