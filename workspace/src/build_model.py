ba3dc5a37b08a975a55408a7aece0ff61d2458da
Steinmetz Arnaud <s.arnaud67@hotmail.fr>
2017-07-11 18:30:52 +0200

    Modifying the model the strictly follow the Unet architecture described in the paper.
    Still need to investigate how the last layer works

--- workspace/src/build_model.py
+++ workspace/src/build_model.py
@@ -13,49 +13,45 @@ def build_UNet2D_4L(inp_shape, k_size=3):
     
     data = Input(shape=inp_shape)
     
-    conv1 = Convolution2D(filters=32, kernel_size=k_size, padding='same', activation='relu')(data)
-    conv1 = Convolution2D(filters=32, kernel_size=k_size, padding='same', activation='relu')(conv1)
+    conv1 = Convolution2D(filters=(64), kernel_size=k_size, padding='same', activation='relu')(data)
+    conv1 = Convolution2D(filters=(64), kernel_size=k_size, padding='same', activation='relu')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-    conv2 = Convolution2D(filters=64, kernel_size=k_size, padding='same', activation='relu')(pool1)
-    conv2 = Convolution2D(filters=64, kernel_size=k_size, padding='same', activation='relu')(conv2)
+    conv2 = Convolution2D(filters=(128), kernel_size=k_size, padding='same', activation='relu')(pool1)
+    conv2 = Convolution2D(filters=(128), kernel_size=k_size, padding='same', activation='relu')(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-    conv3 = Convolution2D(filters=64, kernel_size=k_size, padding='same', activation='relu')(pool2)
-    conv3 = Convolution2D(filters=64, kernel_size=k_size, padding='same', activation='relu')(conv3)
+    conv3 = Convolution2D(filters=(256), kernel_size=k_size, padding='same', activation='relu')(pool2)
+    conv3 = Convolution2D(filters=(256), kernel_size=k_size, padding='same', activation='relu')(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-    conv4 = Convolution2D(filters=128, kernel_size=k_size, padding='same', activation='relu')(pool3)
-    conv4 = Convolution2D(filters=128, kernel_size=k_size, padding='same', activation='relu')(conv4)
+    conv4 = Convolution2D(filters=(512), kernel_size=k_size, padding='same', activation='relu')(pool3)
+    conv4 = Convolution2D(filters=(512), kernel_size=k_size, padding='same', activation='relu')(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
-    conv5 = Convolution2D(filters=256, kernel_size=k_size, padding='same', activation='relu')(pool4)
+    conv5 = Convolution2D(filters=(1024), kernel_size=k_size, padding='same', activation='relu')(pool4)
 
     up1 = UpSampling2D(size=(2, 2))(conv5)
-    conv6 = Convolution2D(filters=256, kernel_size=k_size, padding='same', activation='relu')(up1)
-    conv6 = Convolution2D(filters=256, kernel_size=k_size, padding='same', activation='relu')(conv6)
-    merged1 = concatenate([conv4, conv6], axis=merge_axis)
-    conv6 = Convolution2D(filters=256, kernel_size=k_size, padding='same', activation='relu')(merged1)
+    merged1 = concatenate([conv4, up1], axis=merge_axis)
+    conv6 = Convolution2D(filters=(512), kernel_size=k_size, padding='same', activation='relu')(merged1)
+    conv6 = Convolution2D(filters=(512), kernel_size=k_size, padding='same', activation='relu')(conv6)
 
     up2 = UpSampling2D(size=(2, 2))(conv6)
-    conv7 = Convolution2D(filters=256, kernel_size=k_size, padding='same', activation='relu')(up2)
-    conv7 = Convolution2D(filters=256, kernel_size=k_size, padding='same', activation='relu')(conv7)
-    merged2 = concatenate([conv3, conv7], axis=merge_axis)
-    conv7 = Convolution2D(filters=256, kernel_size=k_size, padding='same', activation='relu')(merged2)
+    merged2 = concatenate([conv3, up2], axis=merge_axis)
+    conv7 = Convolution2D(filters=(256), kernel_size=k_size, padding='same', activation='relu')(merged2)
+    conv7 = Convolution2D(filters=(256), kernel_size=k_size, padding='same', activation='relu')(conv7)
 
     up3 = UpSampling2D(size=(2, 2))(conv7)
-    conv8 = Convolution2D(filters=128, kernel_size=k_size, padding='same', activation='relu')(up3)
-    conv8 = Convolution2D(filters=128, kernel_size=k_size, padding='same', activation='relu')(conv8)
-    merged3 = concatenate([conv2, conv8], axis=merge_axis)
-    conv8 = Convolution2D(filters=128, kernel_size=k_size, padding='same', activation='relu')(merged3)
+    merged3 = concatenate([conv2, up3], axis=merge_axis)
+    conv8 = Convolution2D(filters=(128), kernel_size=k_size, padding='same', activation='relu')(merged3)
+    conv8 = Convolution2D(filters=(128), kernel_size=k_size, padding='same', activation='relu')(conv8)
 
     up4 = UpSampling2D(size=(2, 2))(conv8)
-    conv9 = Convolution2D(filters=64, kernel_size=k_size, padding='same', activation='relu')(up4)
-    conv9 = Convolution2D(filters=64, kernel_size=k_size, padding='same', activation='relu')(conv9)
-    merged4 = concatenate([conv1, conv9], axis=merge_axis)
-    conv9 = Convolution2D(filters=64, kernel_size=k_size, padding='same', activation='relu')(merged4)
+    merged4 = concatenate([conv1, up4], axis=merge_axis)
+    conv9 = Convolution2D(filters=(64), kernel_size=k_size, padding='same', activation='relu')(merged4)
+    conv9 = Convolution2D(filters=(64), kernel_size=k_size, padding='same', activation='relu')(conv9)
 
-    conv10 = Convolution2D(filters=3, kernel_size=k_size, padding='same', activation='sigmoid')(conv9)
+    conv10 = Convolution2D(filters=2, kernel_size=1, padding='same', activation='sigmoid')(conv9)
 
     output = conv10
     model = Model(data, output)
