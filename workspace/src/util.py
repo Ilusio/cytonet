697cef1d05268a72a4943d0c4a8e56dc18842cdc
Jean-Marcellin Truong <jean-marcellin.truong@ensimag.grenoble-inp.fr>
2017-07-18 16:17:52 +0200

    Folders input for segmentation
    You can now use a folder as an input for the segmentation

--- workspace/src/util.py
+++ workspace/src/util.py
@@ -1,5 +1,6 @@
 import os
 import glob
+from skimage import morphology
 
 def mkdirs(folder, permission):
     if not os.path.exists(folder):
@@ -14,3 +15,14 @@ def extend_glob(filenames):
     for filename in filenames:
         files.extend(glob.glob(filename))
     return files
+
+def file_suffix(file, suffix):
+    name = os.path.splitext(os.path.basename(file))[0]
+    new_name = os.path.dirname(file)+"/"+name+suffix
+    return new_name
+
+def remove_small_regions(img, size):
+    """Morphologically removes small (less than size) connected regions of 0s or 1s."""
+    img = morphology.remove_small_objects(img, size)
+    img = morphology.remove_small_holes(img, size)
+    return img
