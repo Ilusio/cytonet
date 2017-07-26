2b294a0ad3940103c891409ecfff5acce6fad75a
Jean-Marcellin Truong <jean-marcellin.truong@ensimag.grenoble-inp.fr>
2017-07-26 16:37:55 +0200

    Extract all the patches on an image

--- workspace/src/extract.py
+++ workspace/src/extract.py
@@ -15,58 +15,7 @@ from skimage import color
 from skimage import io
 import time
 from util import mkdirs
-
-def initValues(files, level, maskPattern, classes):
-    extracting = {} # classes with connected components
-    none_extracting = {} # the number of patches of this class depends on the previous dictionnary
-    max_extraction = 0 # the number of patches for the none classes
-    n_files = len(files)
-    for key, tup in classes.items():
-        val = tup[1]
-        if(val==None):
-            none_extracting[key]=0
-        elif(val<=0):
-            extracting[key]=0
-    # classes cropped by connected components
-    if(len(extracting)>0):
-        for file in files:
-            # reading the images
-            print("Working on " + file)
-            name = os.path.splitext(os.path.basename(file))[0]
-            maskname = glob.glob(os.path.dirname(file)+"/"+name+maskPattern)[0]
-            im = op.OpenSlide(file)
-            mask = Image.open(maskname)
-            if(im.level_dimensions[level] != mask.size):
-               mask = mask.resize(im.level_dimensions[level])
-            maskArray = np.array(mask)
-            del im
-            # find the number of connected components
-            for key, val in extracting.items():
-                nb_patches = (0 - classes[key][1]) + 1 # number of shifts
-                # find connected components
-                maskClass = np.array(maskArray)
-                np.putmask(maskClass,maskClass!=classes[key][0],0)
-                maskClass = measure.label(maskClass)
-                nb_extract = maskClass.max()
-                # take max images or the number of connected components               
-                try:
-                    max_class=classes[key][2]
-                    if(max_class<maskClass.max()):
-                        print("Found ", maskClass.max(), " components for ", key, " but max number is ", max_class)
-                        nb_extract = max_class
-                except:
-                    pass
-                extracting[key] += nb_extract * nb_patches
-                print("New number of extraction for " , key, " : ", extracting[key])
-        # initialize the arrays
-        for key, val in extracting.items():
-            if(val>max_extraction):
-                max_extraction=val
-    # initialize the arrays for none classes
-    for key, val in none_extracting.items():
-        classes[key]=(classes[key][0],int(int(max_extraction/n_files)*classes[key][2]),classes[key][2])
-        print("Number of extraction per file for ", key, " is ", classes[key][1])
-    return classes
+from skimage.util import view_as_blocks
 
 def addBackground(imArray, maskArray):
     """
@@ -122,94 +71,23 @@ def extractPatches(output,filename,maskname, classes, level, patchSize,j):
     
     #Preprocess
     maskArray_back = addBackground(imArray, maskArray)
-    imArray = np.lib.pad(imArray, ((halfPatch, halfPatch), (halfPatch, halfPatch),(0,0)), 'reflect')
-    maskArrayPad = np.lib.pad(maskArray_back, ((halfPatch, halfPatch), (halfPatch, halfPatch)), 'reflect')
+    imArray = np.lib.pad(imArray[:,:,0:3], ((patchSize-(imArray.shape[0]%patchSize), 0), (patchSize-(imArray.shape[1]%patchSize), 0),(0,0)), 'reflect')
+    maskArrayPad = np.lib.pad(maskArray_back, ((patchSize-maskArray_back.shape[0]%patchSize, 0), (patchSize-maskArray_back.shape[1]%patchSize, 0)), 'reflect')
     np.putmask(maskArrayPad, maskArrayPad==1, 255)
-    # Extraction
-    for key, tup in classes.items():
-        print("Extracting patches for ", key)
-        # classes with the number of patches specified
-        img_class = tup[1]
-        if(img_class>0):
-            print("Extracting ", img_class, " patches ")
-            indices = np.where(maskArray_back==tup[0])
-            sample = random.sample(range(len(indices[0])), img_class)
-            maskClass = np.array(maskArrayPad) #TODO : remove this ?  
-            for i in sample:
-                x=indices[0][i]
-                y=indices[1][i]
-                x2 = x+patchSize
-                y2 = y+patchSize
-                croppedIm = imArray[x:x2,y:y2,0:3]
-                croppedMask = maskClass[x:x2,y:y2]              
-                # create the images
-                imageName = output + "/" + key + "/image_" + str(j) + ".png"
-                imageNameMask =  output + "/" + key + "/image_" + str(j) +"_mask.png"
-                misc.imsave(imageName,croppedIm)
-                misc.imsave(imageNameMask,croppedMask)
-                os.chmod(imageName , 0o777)
-                os.chmod(imageNameMask, 0o777)
-                j+=1
-                if(j%100==0):
-                    print("",j," patches extracted")
-        else:
-            # classes with connected components
-            nb_patches = (0 - tup[1]) + 1
-            maskClass = np.array(maskArray_back)
-            np.putmask(maskClass,maskClass!=tup[0],0)
-            maskClass = measure.label(maskClass)
-            bb_labels = measure.regionprops(maskClass)
-            nb_extract = maskClass.max()
-            extract_sample = range(len(bb_labels))
-            try:
-                max_class= tup[2]
-                if(max_class<maskClass.max()):
-                    nb_extract = max_class
-                    extract_sample = random.sample(extract_sample,nb_extract)
-            except:
-                pass  
-            for i in extract_sample:
-                bb = bb_labels[i].bbox
-                x_center = int((bb[0]+bb[2]) / 2) + halfPatch
-                y_center = int((bb[1]+bb[3]) / 2) + halfPatch
-                division = 2
-                shifting = [(0,-patchSize/division),(-patchSize/division,0),(0,patchSize/division),(patchSize/division,0), \
-                            (-patchSize/division,-patchSize/division), (-patchSize/division,patchSize/division), \
-                            (patchSize/division,patchSize/division), (patchSize/division,-patchSize/division)
-                           ]
-                # shifting
-                for h in range(0,nb_patches):
-                    x = x_center
-                    y = y_center
-                    if(h==0):
-                        None
-                    else:
-                        if(len(shifting)>0):
-                            rd = random.randint(0, len(shifting)-1)
-                            shift = shifting[rd]
-                            x+=shift[0]
-                            y+=shift[1]
-                            del shifting[rd]
-                        else:
-                            x += random.randint(-patchSize/division,patchSize/division)     
-                            y += random.randint(-patchSize/division,patchSize/division)
-                    x1 = int(x-patchSize/2)
-                    x2 = int(x+patchSize/2)
-                    y1 = int(y-patchSize/2)
-                    y2 = int(y+patchSize/2)
-                    # cropping
-                    croppedIm = imArray[x1:x2,y1:y2,0:3]
-                    croppedMask = maskArrayPad[x1:x2,y1:y2]
-                    # create the images if needed
-                    imageName = output + "/" + key + "/image_" + str(j) + ".png"
-                    imageNameMask =  output + "/" + key + "/image_" + str(j) +"_mask.png"
-                    misc.imsave(imageName,croppedIm)
-                    misc.imsave(imageNameMask,croppedMask)
-                    os.chmod(imageName , 0o777)
-                    os.chmod(imageNameMask, 0o777)
-                    j+=1
-                    if(j%100==0):
-                        print("",j," patches extracted")
+    images_array = view_as_blocks(imArray, (64,64,3))
+    images_array = images_array.reshape(-1,64,64,3)
+    masks_array = view_as_blocks(maskArrayPad, (64,64))
+    masks_array = masks_array.reshape(-1,64,64)
+    for i in range(masks_array.shape[0]):    
+        imageName = output + "/image_" + str(j) + ".png"
+        imageNameMask =  output + "/image_" + str(j) +"_mask.png"
+        misc.imsave(imageName,images_array[i])
+        misc.imsave(imageNameMask,masks_array[i])
+        os.chmod(imageName , 0o777)
+        os.chmod(imageNameMask, 0o777)
+        j+=1
+        if(j%100==0):
+            print("",j," patches extracted")
     return j
 
 def extractFiles(files,
@@ -222,18 +100,15 @@ def extractFiles(files,
         Extract all the files of a folder
     """
     j = 0
-    new_classes = initValues(files, level, maskPattern, classes)
     for oneFile in files:
         name = os.path.splitext(os.path.basename(oneFile))[0]
-        for key, val in classes.items():
-            folder = os.path.join(outputFolder,key)
-            mkdirs(folder,0o777)
+        mkdirs(outputFolder,0o777)
         print("Extracting " + name)
         maskFile = glob.glob(os.path.dirname(oneFile)+"/"+name+maskPattern)[0]
         j = extractPatches(outputFolder, 
                            oneFile, 
                            maskFile,  
-                           new_classes,  
+                           classes,  
                            level, 
                            patchSize,
                            j)
--- workspace/src/save.py
+++ workspace/src/save.py
@@ -14,29 +14,25 @@ from skimage import morphology
 from skimage import color
 from skimage import io
 
-def load_data(classFolders, files_pattern, patchSize):
+def load_data(files, files_pattern, patchSize):
     """
         Loads patches into a dictionary
     """
-    data = {}
-    for oneClass in classFolders:
-        files=glob.glob(os.path.join(oneClass,files_pattern))
-        className=oneClass.split("/")[-1]        
-        data[className+'_imgs']=np.zeros((len(files),patchSize,patchSize,3)).astype(np.float32)
-        data[className+'_masks']=np.zeros((len(files),patchSize,patchSize,1)).astype(np.float32)
-        i=0
-        for file in files:
+    data = {}    
+    data['imgs']=np.zeros((len(files),patchSize,patchSize,3)).astype(np.float32)
+    data['masks']=np.zeros((len(files),patchSize,patchSize,1)).astype(np.float32)
+    i=0
+    for file in files:
             name = os.path.splitext(os.path.basename(file))[0]
             maskFile = glob.glob(os.path.dirname(file)+"/"+name+"_mask.png")[0]
             mask = Image.open(maskFile)
             img = Image.open(file)
-            imgArray = np.array(img).astype(np.float32)
-            imgArray = (imgArray - imgArray.min())/(imgArray.max() - imgArray.min())
+            imgArray = np.array(img).astype(np.float32)/255
             maskArray = np.expand_dims(np.array(mask),-1).astype(np.float32)
             np.putmask(maskArray,maskArray==128,2)
             np.putmask(maskArray,maskArray==255,1)
-            data[className+'_imgs'][i,:,:,:]=imgArray
-            data[className+'_masks'][i,:,:,:]=maskArray
+            data['imgs'][i,:,:,:]=imgArray
+            data['masks'][i,:,:,:]=maskArray
             i+=1
     return data
 
@@ -44,25 +40,11 @@ def normalizeAndSave(outputFile, data, classes, patchSize):
     """
         Normalize the data and save it in a file
     """
-    j=0
-    for key, val in classes.items():
-        j+=data[key+"_imgs"].shape[0]
-    all_data = np.zeros((j,patchSize,patchSize,3))
-    i = 0
-    # concatenate all the classes in one array to get the mean and std
-    for key, val in classes.items():
-        data[key+"_imgs"] = data[key+"_imgs"].astype(np.float32)
-        data[key+"_masks"] = data[key+"_masks"].astype(np.float32)
-        n = data[key+"_imgs"].shape[0] + i
-        print("Writing from ", i, " to ", n, " for ", key)
-        all_data[i:n,:,:,:]= data[key+"_imgs"]
-        i = n
+    all_data = data['imgs']
     mean = all_data.mean()
     std = all_data.std()
-    
-    for key, val in classes.items():
-        data[key+"_imgs"] -= mean   
-        data[key+"_imgs"] /= std
+    all_data -= mean   
+    all_data /= std
     
     stats = np.zeros(2)
     stats[0] = mean
@@ -73,7 +55,6 @@ def normalizeAndSave(outputFile, data, classes, patchSize):
     # Create the file
     f = h5py.File(outputFile,"w")
     f.create_dataset("stats", data=stats)
-    for key, val in classes.items():
-        f.create_dataset(key+"_imgs", data=data[key+"_imgs"])
-        f.create_dataset(key+"_masks", data=data[key+"_masks"])
+    f.create_dataset("imgs", data=all_data)
+    f.create_dataset("masks", data=data["masks"])
     f.close()
