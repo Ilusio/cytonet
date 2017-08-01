cd114fa9871094a8080621a09eed4ea91da81456
Steinmetz Arnaud <s.arnaud67@hotmail.fr>
2017-08-01 18:15:12 +0200

    Adding UNet model with softmax as last activation layer

--- workspace/src/build_model.py
+++ workspace/src/build_model.py
@@ -2,6 +2,58 @@ from keras.models import Model
 from keras.layers.merge import concatenate
 from keras.layers import Input, Convolution2D, MaxPooling2D, UpSampling2D, Conv2DTranspose, Dropout
 
+def build_UNet_softmax(inp_shape, nb_classes, k_size=3):
+    """
+        Build the unet model
+    """
+    
+    merge_axis = -1 # Feature maps are concatenated along last axis (for tf backend, should be 0 for theano)
+    
+    data = Input(shape=inp_shape)
+    
+    conv1 = Convolution2D(filters=(64), kernel_size=k_size, padding='same', activation='relu')(data)
+    conv1 = Convolution2D(filters=(64), kernel_size=k_size, padding='same', activation='relu')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Convolution2D(filters=(128), kernel_size=k_size, padding='same', activation='relu')(pool1)
+    conv2 = Convolution2D(filters=(128), kernel_size=k_size, padding='same', activation='relu')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Convolution2D(filters=(256), kernel_size=k_size, padding='same', activation='relu')(pool2)
+    conv3 = Convolution2D(filters=(256), kernel_size=k_size, padding='same', activation='relu')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Convolution2D(filters=(512), kernel_size=k_size, padding='same', activation='relu')(pool3)
+    conv4 = Convolution2D(filters=(512), kernel_size=k_size, padding='same', activation='relu')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Convolution2D(filters=(1024), kernel_size=k_size, padding='same', activation='relu')(pool4)
+
+    up1 = UpSampling2D(size=(2, 2))(conv5)
+    merged1 = concatenate([up1,conv4], axis=merge_axis)
+    conv6 = Convolution2D(filters=(512), kernel_size=k_size, padding='same', activation='relu')(merged1)
+    conv6 = Convolution2D(filters=(512), kernel_size=k_size, padding='same', activation='relu')(conv6)
+
+    up2 = UpSampling2D(size=(2, 2))(conv6)
+    merged2 = concatenate([up2,conv3], axis=merge_axis)
+    conv7 = Convolution2D(filters=(256), kernel_size=k_size, padding='same', activation='relu')(merged2)
+    conv7 = Convolution2D(filters=(256), kernel_size=k_size, padding='same', activation='relu')(conv7)
+
+    up3 = UpSampling2D(size=(2, 2))(conv7)
+    merged3 = concatenate([up3,conv2], axis=merge_axis)
+    conv8 = Convolution2D(filters=(128), kernel_size=k_size, padding='same', activation='relu')(merged3)
+    conv8 = Convolution2D(filters=(128), kernel_size=k_size, padding='same', activation='relu')(conv8)
+
+    up4 = UpSampling2D(size=(2, 2))(conv8)
+    merged4 = concatenate([up4,conv1], axis=merge_axis)
+    conv9 = Convolution2D(filters=(64), kernel_size=k_size, padding='same', activation='relu')(merged4)
+    conv9 = Convolution2D(filters=(64), kernel_size=k_size, padding='same', activation='relu')(conv9)
+
+    conv10 = Convolution2D(filters=nb_classes, kernel_size=1, padding='same', activation='softmax')(conv9)
+
+    output = conv10
+    model = Model(data, output)
+    return model
 
 def build_UNet(inp_shape, nb_classes, k_size=3):
     """
